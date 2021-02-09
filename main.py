dd12b667fd823e74f0cddfc29643d7ec02884b24
Philipp Homberger <philipp.homberger@gmail.com>
2021-02-09 21:37:43 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,8 @@
 # !/usr/bin/python
 import requests
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
-import xml.etree.ElementTree as ET
 import json
 import sys
-
-# Returns list of all the project names
-def get_projects():
-    global PROPERTIES
-    global HEADERS
-    project_names = []
-    try:
-        url = URL + 'projects'
-        r = requests.get(url, headers=HEADERS, verify=False, timeout=PROPERTIES['TIMEOUT'])
-        root = ET.fromstring(r.text.encode('utf-8'))
-        for project in root:
-            for name in project.findall('name'):
-                project_names.append(name.text)
-                print(name.text)
-    except:
-        print("Problem with project listing {0}".format(r))
-        pass
-    return project_names
-
 
 # API call to delete an execution by ID
 def delete_execution(execution_id):
@@ -31,16 +11,13 @@ def delete_execution(execution_id):
     url = URL + 'execution/' + execution_id
     try:
         r = requests.delete(url, headers=HEADERS, verify=False, timeout=PROPERTIES['TIMEOUT'])
-        if PROPERTIES['VERBOSE']:
-            print
-            "            Deleted execution id {0} {1} {2}".format(execution_id, r.text, r)
     except:
         pass
 
 
 # API call to bulk delete executions by ID
 def delete_executions():
-    werte = list(range(10, 100))
+    werte = list(range(0, 5500))
     global PROPERTIES
     global HEADERS
     url = URL + 'executions/delete'
